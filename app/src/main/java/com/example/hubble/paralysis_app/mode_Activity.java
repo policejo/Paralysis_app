b112e450a62ffcd9f3aad401225b28277097b681
policejo <hackgmt@gmail.com>
2017-04-06 22:04:38 +0700

    update

--- app/src/main/java/com/example/hubble/paralysis_app/mode_Activity.java
+++ app/src/main/java/com/example/hubble/paralysis_app/mode_Activity.java
@@ -3,20 +3,68 @@ package com.example.hubble.paralysis_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class mode_Activity extends AppCompatActivity {
+
+    private RecyclerView recyclerviwe;
+    private LinearLayoutManager recyclerviwemanager;
+    private Recyeviwe_adapter madapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_);
+        recyclerviwe = (RecyclerView)findViewById(R.id.rec);
+        recyclerviwe = (RecyclerView)findViewById(R.id.rec);
+        recyclerviwe.setHasFixedSize(true);
+
+        recyclerviwemanager = new LinearLayoutManager(this);
+        recyclerviwe.setLayoutManager(recyclerviwemanager);
+        madapter = new Recyeviwe_adapter();
+        recyclerviwe.setAdapter(madapter);
     }
 
-    public void Action_1(View view) {
-        // เปิดหน้า activity ขึ้นใหม่โดยไม่ปิด activity เดิม
-        // เปิด ท่าที่ 1  สำหรับใช้ Accelerometer
-        Intent i = new Intent(getApplicationContext(), _action_1.class);
-        startActivity(i);
+    public  class Recyeviwe_adapter extends  RecyclerView.Adapter<ViweHolder>{
+
+
+        @Override
+        public ViweHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item,parent,false);
+            return new ViweHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViweHolder holder, int position) {
+            holder.tv_action_name.setText("ชื่อท่า" + position);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 6;
+        }
     }
+
+    public  class   ViweHolder extends RecyclerView.ViewHolder{
+                TextView tv_action_name;
+                ImageView imageth;
+                TextView tv_action_detail;
+                public ViweHolder(View itemView) {
+                    super(itemView);
+                    tv_action_name = (TextView) itemView.findViewById(R.id.tv_action_name);
+                    tv_action_detail = (TextView) itemView.findViewById(R.id.tv_action_detail);
+                    imageth = (ImageView) itemView.findViewById(R.id.imageth);
+
+                }
+            }
+
+
 }
